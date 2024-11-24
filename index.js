671b01291c62d80f6415206c52f78ae9aacc6c7b
wftb <145103446+wftb@users.noreply.github.com>
2024-11-24 12:02:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 
-//const proxyIPs = ['ProxyIP.Oracle.fxxk.dedyn.io', 'ProxyIP.US.fxxk.dedyn.io'];
+const proxyIPs = ['ProxyIP.Oracle.fxxk.dedyn.io', 'ProxyIP.US.fxxk.dedyn.io'];
 
 // if you want to use ipv6 or single proxyIP, please add comment at this line and remove comment at the next line
-//let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
+let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 // how to make sure the proxyIP with port is valid?
 // go to https://proxyip.edtunnel.best/ , input your proxyIP:proxyPort, and click "Check" button
 // if the port is valid, value "Proxy IP" is true, otherwise false and the value "Origin" must be 443
 let proxyPort = proxyIP.includes(':') ? proxyIP.split(':')[1] : '443';
 
 // use single proxyip instead of random
-let proxyIP = 'ProxyIP.US.fxxk.dedyn.io';
+// let proxyIP = 'ProxyIP.US.fxxk.dedyn.io';
 // ipv6 proxyIP example remove comment to use
 // let proxyIP = "[2a01:4f8:c2c:123f:64:5:6810:c55a]"
 
